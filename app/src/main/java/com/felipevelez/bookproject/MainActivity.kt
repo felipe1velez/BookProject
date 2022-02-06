1002fbad9ff646a8247465d1eba87dfcee89ba39
felipe velez <duvanf.velez@udea.edu.co>
2022-02-05 19:51:03 -0500

    Commit interfaces sin terminar

--- app/src/main/java/com/felipevelez/bookproject/MainActivity.kt
+++ app/src/main/java/com/felipevelez/bookproject/MainActivity.kt
@@ -1,29 +1,43 @@
 package com.felipevelez.bookproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.felipevelez.bookproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
 
-        val nameBookEditText : EditText = findViewById(R.id.name_book_edit_text)
-        val saveButton : Button = findViewById(R.id.save_button)
-        val infoTextView : TextView = findViewById(R.id.info_text_view)
-        val nameAuthorEditText : EditText = findViewById(R.id.name_author_edit_text)
+        setContentView(mainBinding.root)
 
-        saveButton.setOnClickListener {
-            val nameBook : String = nameBookEditText.text.toString()
-            infoTextView.text = nameBook
-            val nameAuthor : String = nameAuthorEditText.text.toString()
-            infoTextView.text = nameAuthor
+        //  val nameBookEditText : EditText = findViewById(R.id.name_book_edit_text)
 
+        // var numero: Int? = null
+       // numero = null
 
-            
+        with(mainBinding) {
+            saveButton.setOnClickListener {
+
+                if (nameBookEditText.text.isEmpty() ||
+                    nameAuthorEditText.text.isEmpty() ||
+                    pagesEditText.text?.isEmpty() == true
+                ) {
+                    Toast.makeText(applicationContext, "Debe digitar nombre, autor y número de páginas", Toast.LENGTH_SHORT).show()
+
+                } else {
+                    val nameBook: String = nameBookEditText.text.toString()
+                    val author = nameAuthorEditText.text.toString()
+                    val pages = pagesEditText.text.toString().toInt()
+                    val abstract = abstractEditText.text.toString()
+
+                    infoTextView.text =getString(R.string.info, nameBook, author, pages, abstract)
+                }
+            }
         }
     }
 }
