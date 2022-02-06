b45cc8fba2459f89bb2f41faf8c9c6bec9945006
felipe velez <duvanf.velez@udea.edu.co>
2022-02-06 18:59:24 -0500

    Commit interfaces estandarizadas splash 70%

--- app/src/main/java/com/felipevelez/bookproject/SplashActivity.kt
+++ app/src/main/java/com/felipevelez/bookproject/SplashActivity.kt
@@ -1,16 +1,15 @@
 package com.felipevelez.bookproject
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.felipevelez.bookproject.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
 import com.felipevelez.bookproject.databinding.ActivitySplashBinding
 import java.util.*
 import kotlin.concurrent.timerTask
 
 class SplashActivity : AppCompatActivity() {
 
-    private lateinit var splashBinding :ActivitySplashBinding
+    private lateinit var splashBinding: ActivitySplashBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,18 +17,16 @@ class SplashActivity : AppCompatActivity() {
         setContentView(splashBinding.root)
 
         val timer = Timer()
-        timer.schedule(timerTask {
-            goToMainActivity()
-        },2000
-        ) 
+        timer.schedule(
+            timerTask {
+                goToMainActivity()
+            }, 2000
+        )
+    }
 
-        splashBinding.imageView.setOnClickListener { //metodo de views
-            val intent = Intent(this, MainActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            startActivity(intent)
-           // finish() //destruye la actividad
-
-        }
-
+    private fun goToMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
     }
 }
