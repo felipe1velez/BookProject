2937245cc070ed31bdaf599dae564e59acf1b777
felipe velez <duvanf.velez@udea.edu.co>
2022-02-06 02:43:21 -0500

    Commit interfaces sin estandarizar

--- app/src/main/java/com/felipevelez/bookproject/MainActivity.kt
+++ app/src/main/java/com/felipevelez/bookproject/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.felipevelez.bookproject
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.felipevelez.bookproject.databinding.ActivityMainBinding
+import java.lang.ClassCastException
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mainBinding: ActivityMainBinding
-
+    private var cal = Calendar.getInstance()
+    private var publicationDate = ""
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
@@ -17,17 +24,36 @@ class MainActivity : AppCompatActivity() {
 
         //  val nameBookEditText : EditText = findViewById(R.id.name_book_edit_text)
 
-        // var numero: Int? = null
-       // numero = null
+        val dateSetListener = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, month)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
 
+            val format = "dd/MM/yyyy"
+            val simpleDateFormat = SimpleDateFormat(format, Locale.US)
+            publicationDate = simpleDateFormat.format(cal.time).toString()
+            mainBinding.publicationDateButton.text = publicationDate
+        }
+        
         with(mainBinding) {
+            
+            publicationDateButton.setOnClickListener { 
+                DatePickerDialog(
+                    this@MainActivity,
+                    dateSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)
+                ).show()
+            }
+            
             saveButton.setOnClickListener {
 
                 if (nameBookEditText.text.isEmpty() ||
                     nameAuthorEditText.text.isEmpty() ||
                     pagesEditText.text?.isEmpty() == true
                 ) {
-                    Toast.makeText(applicationContext, "Debe digitar nombre, autor y número de páginas", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(applicationContext, "Debe digitar nombre, autor y el número de páginas", Toast.LENGTH_SHORT).show()
 
                 } else {
                     val nameBook: String = nameBookEditText.text.toString()
@@ -35,7 +61,24 @@ class MainActivity : AppCompatActivity() {
                     val pages = pagesEditText.text.toString().toInt()
                     val abstract = abstractEditText.text.toString()
 
-                    infoTextView.text =getString(R.string.info, nameBook, author, pages, abstract)
+
+                    var genre = ""
+
+                    if(suspenseCheckBox.isChecked) genre = "Suspenso"
+                    if(terrorCheckBox.isChecked) genre += " Terror"
+                    if(infantileCheckBox.isChecked) genre += " Infantil"
+                    if(fictionCheclBox.isChecked) genre += " Ficción"
+
+                    val score = when { //depronto es var
+                        oneRadioButton.isChecked -> 1
+                        twoRadioButton.isChecked -> 2
+                        threeRadioButton.isChecked -> 3
+                        fourRadioButton.isChecked -> 4
+                        else -> 5
+                    }
+
+
+                    infoTextView.text =getString(R.string.info, nameBook, author, pages, abstract, genre, score, publicationDate)
                 }
             }
         }
